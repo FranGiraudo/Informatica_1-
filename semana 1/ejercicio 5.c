453a7829e70e65fe90777f460ca00077c61a87b2
FranGiraudo <132246286+FranGiraudo@users.noreply.github.com>
2023-06-04 20:26:01 -0300

    Update ejercicio 5.c

--- semana 1/ejercicio 5.c
+++ semana 1/ejercicio 5.c
@@ -5,8 +5,8 @@ int main(int argc, char *argv[]) {
 	float cat1=0, cat2=0, hip=0;
 	printf("ingrese el valor de los catetos \n");
 	scanf("%f %f",&cat1, &cat2);
-	hip=sqrt(cat1*cat1+cat2*cat2);
-	printf("el valor de la hipotenusa es:%f",hip);
+	hip=sqrt(pow(2,cat1)+cat2*cat2);
+	printf("el valor de la hipotenusa es:%.2f",hip);
 	return 0;
 }
 
